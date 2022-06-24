85837dd47c8523b32102fd3103eb535fffc5e9bb
Simone Levy <97460770+simolevy@users.noreply.github.com>
2022-06-24 16:14:34 -0400

    Update e_arrow.c

--- src/e_arrow.c
+++ src/e_arrow.c
@@ -131,6 +131,10 @@ add_linearrow(F_line *line, F_point *prev_point, F_point *selected_point)
     set_last_selectedpoint(selected_point);
     set_latestline(line);
     set_action_object(F_ADD_ARROW_HEAD, O_POLYLINE);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
@@ -157,6 +161,10 @@ add_arcarrow(F_arc *arc, int point_num)
     set_last_arcpointnum(point_num);
     set_latestarc(arc);
     set_action_object(F_ADD_ARROW_HEAD, O_ARC);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
@@ -179,6 +187,10 @@ add_splinearrow(F_spline *spline, F_point *prev_point, F_point *selected_point)
     set_last_selectedpoint(selected_point);
     set_latestspline(spline);
     set_action_object(F_ADD_ARROW_HEAD, O_SPLINE);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
@@ -215,6 +227,10 @@ delete_linearrow(F_line *line, F_point *prev_point, F_point *selected_point)
     set_last_selectedpoint(selected_point);
     set_latestline(line);
     set_action_object(F_DELETE_ARROW_HEAD, O_POLYLINE);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
@@ -249,6 +265,10 @@ delete_arcarrow(F_arc *arc, int point_num)
     set_last_arcpointnum(point_num);
     set_latestarc(arc);
     set_action_object(F_DELETE_ARROW_HEAD, O_ARC);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
@@ -284,6 +304,10 @@ delete_splinearrow(F_spline *spline, F_point *prev_point, F_point *selected_poin
     set_last_selectedpoint(selected_point);
     set_latestspline(spline);
     set_action_object(F_DELETE_ARROW_HEAD, O_SPLINE);
+
+	//undo redo
+	undo_update_history();
+
     set_modifiedflag();
 }
 
