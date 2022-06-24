c8abec15518b67ad1d160bb709c63281394a7723
rtrinhh <85720584+rtrinhh@users.noreply.github.com>
2022-06-24 11:32:09 -0400

    Added #quickColoring task to extend access
    1 task total; 1 line

--- src/w_icons.h
+++ src/w_icons.h
@@ -133,6 +133,13 @@ extern icon_struct	tangent_ic;
 extern icon_struct	anglemeas_ic;
 extern icon_struct	lenmeas_ic;
 extern icon_struct	areameas_ic;
+/*------------------------------------Code Starts Here------------------------*/
+// #quickColoring
+// The objcolor variable behaviors can only be accessed within the w-icons file.
+// Declare and extend the objcolor icon strcuture so that the access is across 
+// the Xfig folder.
+
+/*------------------------------------Code Ends Here--------------------------*/
 
 /* misc icons */
 
