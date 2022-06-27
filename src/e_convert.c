23a8cafa7fd17c86caa8925bbf484d6319e78e47
Simone Levy <97460770+simolevy@users.noreply.github.com>
2022-06-27 16:17:08 -0400

    Update e_convert.c

--- src/e_convert.c
+++ src/e_convert.c
@@ -34,6 +34,11 @@
 #include "u_markers.h"
 #include "u_redraw.h"
 #include "u_search.h"
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Include the header file for the undo function. 
+
+/*------------------------------------Code Ends Here--------------------------*/
 #include "u_undo.h"
 #include "w_canvas.h"
 #include "w_cursor.h"
@@ -142,8 +147,12 @@ box_2_box(F_line *old_l)
     /* save pointer to this line for undo */
     latest_line = new_l;
 
-	//undo redo
-	undo_update_history();
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to update the undo history.
+
+/*------------------------------------Code Ends Here--------------------------*/
+    undo_update_history();
 	
     redisplay_line(new_l);
     return;
@@ -216,8 +225,12 @@ line_spline(F_line *l, int type_value)
     set_action_object(F_CONVERT, O_POLYLINE);
     set_latestspline(s);
 
-	//undo redo
-	undo_update_history();
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to update the undo history.
+
+/*------------------------------------Code Ends Here--------------------------*/
+    undo_update_history();
 
     set_modifiedflag();
 }
@@ -234,8 +247,12 @@ spline_line(F_spline *s)
 	return;
 	}
 
-	//undo redo
-	set_freeze_undo_additions(True);
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to freeze additional undo actions and set the value to True.
+
+/*------------------------------------Code Ends Here--------------------------*/
+    set_freeze_undo_additions(True);
 
     if (open_spline(s)) {
 	l->type = T_POLYLINE;
@@ -281,8 +298,12 @@ spline_line(F_spline *s)
 	l->back_arrow = NULL;
     }
 	
-	//undo redo
-	set_freeze_undo_additions(True);
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to freeze additional undo actions and set the value to True.
+
+/*------------------------------------Code Ends Here--------------------------*/
+    set_freeze_undo_additions(True);
 
     /* now we have finished creating the line, we can get rid of the spline */
     delete_spline(s);
@@ -294,7 +315,12 @@ spline_line(F_spline *s)
     set_action_object(F_CONVERT, O_SPLINE);
     set_latestline(l);
 
-	//undo redo
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to freeze additional undo actions and set the value to False.
+// Call the function to update the undo history.
+/*------------------------------------Code Ends Here--------------------------*/
+	
 	set_freeze_undo_additions(False);
 	undo_update_history();
 
@@ -351,7 +377,11 @@ toggle_polyline_polygon(F_line *line, F_point *previous_point, F_point *selected
   set_last_prevpoint(NULL);
   set_latestline(line);
 
-  //undo redo
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to update the undo history.
+
+/*------------------------------------Code Ends Here--------------------------*/
   undo_update_history();
 
   set_modifiedflag();
@@ -441,7 +471,11 @@ toggle_open_closed_spline(F_spline *spline, F_point *previous_point, F_point *se
   set_last_prevpoint(NULL);
   set_latestspline(spline);
 
-  //undo redo
+/*------------------------------------Code Starts Here------------------------*/
+// #task8
+// Call the function to update the undo history.
+
+/*------------------------------------Code Ends Here--------------------------*/
   undo_update_history();
 
   set_modifiedflag();
