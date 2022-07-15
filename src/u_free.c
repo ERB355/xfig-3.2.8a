d4965e45416b339ae9527aa726921a7900e1c494
Simone Levy <97460770+simolevy@users.noreply.github.com>
2022-07-15 09:31:52 -0400

    #task8 solutions removed

--- src/u_free.c
+++ src/u_free.c
@@ -34,7 +34,6 @@
 // Include the header file for mode. 
 
 /*------------------------------------Code Ends Here--------------------------*/
-#include "mode.h"
 
 
 void free_arc(F_arc **list)
@@ -95,25 +94,7 @@ void free_compound(F_compound **list)
 /*------------------------------------Code Ends Here--------------------------*/
 void free_history(F_history **list)
 {
-	if((*list) == NULL)
-		return;
-
-	F_history *h, *history;
 	
-	for (h = *list; h != NULL;) {
-	history = h;
-	h = h->next;
-	//only free history if last action is add. Don't free if object was moved/scaled/etc.
-	if(history->last_action == F_ADD)
-	{
-		free_compound(&history->saved_objects);
-		free_points(history->last_prev_point);
-		free_points(history->last_selected_point);
-	}
-	free((char *) history);
-
-	}
-	*list = NULL;
 }
 
 void free_ellipse(F_ellipse **list)
