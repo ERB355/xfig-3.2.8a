2f7fad2fba198082a7c38a0d4d6c1fce1bf1e3c0
CMSC 355 <99101935+ERB355@users.noreply.github.com>
2022-09-23 02:44:58 +0000

    changes

--- src/e_rotate.c
+++ src/e_rotate.c
@@ -530,7 +530,7 @@ int valid_rot_angle(F_compound *c)
     F_compound *c1;
     
 
-    if (0<=fabs((act_rotnangle % 360.0)*360.0)<=360.0)
+    if (0<=fabs((act_rotnangle % (float)360)*360.0)<=360.0)
         return 1;
     else if (!valid_rot_angle(c1))
         return 0;
